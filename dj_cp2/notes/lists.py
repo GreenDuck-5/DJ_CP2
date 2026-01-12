307037b30add37275c910c852c29a455d23918ba
GreenDuck-5 <dirk.jones@ucas-edu.net>
2026-01-12 09:32:30 -0700

    lists

---
+++ dj_cp2/notes/lists.py
@@ -0,0 +1,8 @@
+#list
+fruit_one = ["apple", "orange", "peach", "kiwi", "raspberry"]
+
+#tuple
+fruit_two = ("apple", "orange", "peach", "kiwi", "raspberry")
+
+#set
+fruit_three = {"apple", "orange", "peach", "kiwi", "raspberry"}
