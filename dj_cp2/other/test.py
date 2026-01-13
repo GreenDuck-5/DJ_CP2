fe0d6b929c17ee52a2a22b0499cf3411be9a2147
GreenDuck-5 <dirk.jones@ucas-edu.net>
2026-01-13 09:10:28 -0700

    dfgh

--- dj_cp2/other/test.py
+++ dj_cp2/other/test.py
@@ -1,18 +1,10 @@
 import os
-def clear_screen():
-    if os.name == 'nt':
-        os.system('CLS')
-    else:
-        os.system('clear')
+def clear_screen(): os.system('CLS') if os.name == 'nt' else os.system('clear')
 
 clear_screen()
 
 _ = 10
 
 for item in range(_):
-    print(item+1)
+    print(item + 1)
 
-minecraft = 100
-print(minecraft)
-minecraft /= 10
-print(minecraft)
