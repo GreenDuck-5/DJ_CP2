d862923085b376ddfb6a704930b34a9ef2b86f25
GreenDuck-5 <dirk.jones@ucas-edu.net>
2026-01-14 09:52:25 -0700

    hdfah

---
+++ dj_cp2/individual_assignments/personal_library.py
@@ -0,0 +1,40 @@
+#DJ, 1st, Personal Library
+
+# movies function
+    #set with all the movies
+    #ask user what they want to do: view, add, search/remove, exit
+
+        #if the user picks to view
+            #if there are now movies:
+                #print: yuo have nothing to veiew
+            #else:
+                #print each movie individually on a new line
+
+        #if the user chooses to add:
+            #ask user what the director is
+            #ask user what the name of the movie is
+            #add movie to set with all the movies
+
+        #if the user chooses to search
+            #let user search
+            #if the search contains the name of the movie, or th ename of the director, print all movies that contain the search
+
+
+
+
+# overarching function
+
+    #Ask user which library they want to access: books, movies, music, exit program
+
+    #if the user picks movies:
+        #launch movie fucnction
+        #
+
+    #if the user picks books:
+        #launch book function
+
+    #if the user picks music:
+        #launch music function
+
+    #if the user picks exit program
+        #end program
