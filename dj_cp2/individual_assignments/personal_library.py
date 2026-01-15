2b15bc9610e3d6a90599c1f2f144a1b0ebf963b6
GreenDuck-5 <dirk.jones@ucas-edu.net>
2026-01-15 09:32:00 -0700

    sfjhgfd

--- dj_cp2/individual_assignments/personal_library.py
+++ dj_cp2/individual_assignments/personal_library.py
@@ -1,40 +1,20 @@
 #DJ, 1st, Personal Library
 
-# movies function
-    #set with all the movies
-    #ask user what they want to do: view, add, search/remove, exit
+#importing extr afunctions
+import time as t
 
-        #if the user picks to view
-            #if there are now movies:
-                #print: yuo have nothing to veiew
-            #else:
-                #print each movie individually on a new line
+#cclearing the screen
+def clear_screen(): print("\033c", end="")
 
-        #if the user chooses to add:
-            #ask user what the director is
-            #ask user what the name of the movie is
-            #add movie to set with all the movies
+#Letting user pick when to move on
+def continue_screen(): input("Press \"Enter\" or \"Return\" to continue:\n")
 
-        #if the user chooses to search
-            #let user search
-            #if the search contains the name of the movie, or th ename of the director, print all movies that contain the search
+#list with the 
 
+#intoroduce the user, give options of what to do: add, search, remove, view, exit
 
-
-
-# overarching function
-
-    #Ask user which library they want to access: books, movies, music, exit program
-
-    #if the user picks movies:
-        #launch movie fucnction
-        #
-
-    #if the user picks books:
-        #launch book function
-
-    #if the user picks music:
-        #launch music function
-
-    #if the user picks exit program
-        #end program
+#if the user picks "add":
+    #common_name = user input: what is the common name
+    #scientific_name = user inpout: whgat is the scienetic mname
+    #print: common name: comomn_name
+    #print: scientific name: scientific_name
