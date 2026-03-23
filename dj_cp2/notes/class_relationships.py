911b85bb2bb5616a9528b0f7abc39696ebb62c5c
GreenDuck-5 <dirk.jones@ucas-edu.net>
2026-03-23 09:02:34 -0600

    class relationships notes

---
+++ dj_cp2/notes/class_relationships.py
@@ -0,0 +1,19 @@
+#What is polymorphism?
+
+#What is inheritance?
+  
+#What do we call the classes in an inheritance relationship?
+
+#How does inheritance simplify your code?
+
+#What is aggregation? 
+
+#How do you use aggregation?
+
+#How does aggregation simplify your code?
+
+#What is composition?
+
+#How do you use composition?
+
+#How does composition simplify your code?
