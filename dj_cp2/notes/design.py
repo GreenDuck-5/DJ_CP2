45e365cbeec1f44d270cd0a3aa3a5861cf5a3c48
GreenDuck-5 <dirk.jones@ucas-edu.net>
2026-04-14 09:35:53 -0600

    design notes

---
+++ dj_cp2/notes/design.py
@@ -0,0 +1,42 @@
+#DJ, 1st, Design Notes
+
+
+#What is top down design?
+    # Plan first, code second
+
+
+#What are the steps of top down design?
+    # Begin with purpose of a projcet
+    # Then break down project into to smaller pieces
+    # Every smaller piece is unique
+
+
+#What are the advantages of top down design?
+    # Allows for specialized teams to take on tasks and collaborate
+    # Makes sure you're not repeating code
+    # Makes it easier to solve programming problems
+    # Easy to track progress
+    # Steps become simple
+    # Reusable solutions
+
+
+#What is bottom up design?
+    # Build small parts, build up to project
+
+
+#What are the steps of bottom up design
+    # Build small parts
+    # Add new parts as they are needed
+    # All the small parts put together to final project
+
+
+#What are the advantage of bottom up design?
+    # Easy to test individual parts
+    # Focus on developers problem solving
+    # Increased modularity
+    # Reusable sections of code
+    # Less time in planning. more time programming
+
+
+#When would it be a good idea to use bottom up design compared to top down design?
+    # Bottom up is useful for smaller, more consise programs
